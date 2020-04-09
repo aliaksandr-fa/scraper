61650fe5121964e1947b5c481dac6ca3ce941d4c
Aliaksandr Falei <aliaksandr.fa.mi.re.do@gmail.com>
2020-04-09 15:53:12 +0300

    dist folder removed

--- dist/src/scraper/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Scraper_1 = require("./Scraper");
-exports.Scraper = Scraper_1.default;
-//# sourceMappingURL=index.js.map
--- dist/src/scraper/inputs/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ScraperRangeInput_1 = require("./ScraperRangeInput");
-exports.ScraperRangeInput = ScraperRangeInput_1.default;
-//# sourceMappingURL=index.js.map
